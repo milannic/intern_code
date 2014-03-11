627abd700e6203bc1a9af70bfa725a5d5fc11089
Milannic <milannic.cheng.liu@gmail.com>
2014-03-11 16:41:14 -0400

    path sum2

--- intern_course/cpp/helloworld.cpp
+++ intern_course/cpp/helloworld.cpp
@@ -6,10 +6,7 @@
  *    Description:  Test
  *
  *        Version:  1.0
- *        Created:  03/10/2014 15:51:27
- *       Revision:  none
- *       Compiler:  gcc
- *
+ *        Created:  03/10/2014 15:51:27 *       Revision:  none *       Compiler:  gcc *
  *         Author:  YOUR NAME (), 
  *   Organization:  
  *
@@ -18,8 +15,12 @@
 
 #include <iostream>
 #include <stdlib.h>
+#include <cstdlib>
 #include <vector>
 #include <stack>
+#include <unordered_set>
+#include <unordered_map>
+#include <queue>
 using namespace std;
 
 /* 
@@ -38,6 +39,46 @@ struct TreeNode {
 
 class Solution {
 public:
+    int ladderLength(string start, string end, unordered_set<string> &dict) {
+		if(start.length()!=end.length()){
+			return 0;
+		}else{
+			int depth = 0;
+			string cur;
+			string temp_string;
+			queue<string> BFS_queue;
+			unordered_map<string,int> visit;
+			BFS_queue.push(start);
+			visit.insert(std::make_pair(start,depth));
+			//BFS Traverse
+			while(!BFS_queue.empty()){
+//				cout<<BFS_queue.size()<<endl;
+//				cout<<visit.size()<<endl;
+				cur = BFS_queue.front();
+				BFS_queue.pop();
+				depth = visit.find(cur)->second;
+//				cout<<cur<<endl;
+//				getchar();
+				for(unsigned i=0;i<cur.length();i++){
+					for(int j=0;j<26;j++){
+						temp_string = cur;
+						char temp_char = 'a' + j;
+						temp_string[i]=temp_char;
+						// this node has not been visited and it exists in the dict
+						if(temp_string.compare(end)==0){return depth+2;}
+						if(visit.find(temp_string)==visit.end() && dict.find(temp_string)!=dict.end()){
+							//cout<<"I am here"<<endl;
+							//cout<<temp_string<<endl;
+							//getchar();
+							visit.insert(std::make_pair(temp_string,depth+1));
+							BFS_queue.push(temp_string);
+						}
+					}
+				}
+			}
+			return 0;
+		}
+    };
 	//leetcode merge sorted array.
     void merge(int A[], int m, int B[], int n) {
 		int index1=m-1;
@@ -176,20 +217,64 @@ public:
 			return false;
 		}
     }
+
+    vector<vector<int> > pathSum(TreeNode *root, int sum){
+		vector<vector <int> > solutions;
+		if(root==NULL){
+			;
+		}
+		else{
+			vector<int>* temp_solution = new vector<int>();
+			vector<int>* temp_solution2=NULL;
+			int temp_sum=root->val;
+			TreeNode* previous=NULL;
+			TreeNode* cur=NULL;
+			stack<TreeNode*> my_stack;
+			my_stack.push(root);
+			temp_solution->push_back(root->val);
+			while(!my_stack.empty()){
+				cur = my_stack.top();
+				if(temp_sum==sum && cur->left==NULL && cur->right==NULL){
+					temp_solution2 = new vector<int>();
+					*temp_solution2 = *temp_solution;
+					solutions.push_back(*temp_solution2);
+				}
+				//leaf node
+				if(cur->left!=NULL && previous!=cur->left && (!(previous == cur->right && cur->right != NULL))){
+						my_stack.push(cur->left);
+						temp_solution->push_back(cur->left->val);
+						temp_sum += cur->left->val;
+
+				}else if(cur->right!=NULL && previous != cur->right){
+					my_stack.push(cur->right);
+					temp_sum += cur->right->val;
+					temp_solution->push_back(cur->right->val);
+				}else{
+					temp_sum -= cur->val;
+					temp_solution->pop_back();
+					previous = my_stack.top();
+					my_stack.pop();
+				}
+			}
+		}
+		return solutions;
+	}
 };
 
 int
 main ( int argc, char *argv[] )
 {
 	Solution* my_solution = new Solution();
-	stack<TreeNode> my_stack;
-	TreeNode* tree1 = new TreeNode(1);
-	TreeNode* tree2 = new TreeNode(-2);
-	TreeNode* tree3 = new TreeNode(3);
-	tree1->left = tree2;
-	tree1->right = tree3;
-	std::cout<<my_solution->hasPathSum(tree1,2)<<endl;
-
+	// test of string
+	string start = "hit";
+	string end = "cog";
+	unordered_set<string> dict;
+	dict.insert("hot");
+	dict.insert("dot");
+	dict.insert("dog");
+	dict.insert("lot");
+	dict.insert("log");
+	cout<<my_solution->ladderLength(start,end,dict)<<endl;
 	return EXIT_SUCCESS;
 }				/* ----------  end of function main  ---------- */
 
